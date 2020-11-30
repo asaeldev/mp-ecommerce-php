f1a690ff1d7d8628f45e6fc56f0f7a32f6fc016e
asaeldev <asael.dev@gmail.com>
2020-11-29 19:23:55 -0600

    Changed logic for logs
    Added new logic for saving notification logs differently

--- notification.php
+++ notification.php
@@ -10,9 +10,9 @@ $json = file_get_contents("php://input");
 $data = json_decode($json);
 
 if (isset($data->type)) {
-    $filename = "./logs/" . $data->type . '_' . $data->id . '.txt';
-    $file = fopen($filename, "w");
-    fwrite($file, json_encode($data));
+    $filename = "./logs/logs.txt";
+    $file = fopen($filename, "a");
+    fwrite($file, json_encode($data) . PHP_EOL);
 
     switch ($data->type) {
         case "payment":
